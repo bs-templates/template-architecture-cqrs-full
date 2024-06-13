668060d2e605c16f948fe9474e2f359d49b4aa3d
Italo Silveira <italobrian@gmail.com>
2024-06-13 19:46:26 -0300

    feature: api project and initial config

---
+++ src/BAYSOFT.Infrastructures.Data/Default/Migrations/20240613224319_InitialMigrationDefaultDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BAYSOFT.Infrastructures.Data.Default.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationDefaultDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Samples",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(512)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samples", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Samples",
+                schema: "dbo");
+        }
+    }
+}
